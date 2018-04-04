0ee280dde5cc2454e62b8a204ad8a046f98ac519
ciglesiascrespo <ciglesiascrespo@gmail.com>
2018-04-03 22:44:27 -0500

    se agrega pantalla de información del usuario

--- app/src/main/java/com/iglesias/c/appgym/Activity/MainActivity.java
+++ app/src/main/java/com/iglesias/c/appgym/Activity/MainActivity.java
@@ -1,12 +1,25 @@
 package com.iglesias.c.appgym.Activity;
 
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.iglesias.c.appgym.R;
+import com.iglesias.c.appgym.Service.UsbService;
 import com.squareup.picasso.Picasso;
+
+import java.util.Set;
 
 import static com.iglesias.c.appgym.Activity.LoginActivity.EXTRA_DIAS;
 import static com.iglesias.c.appgym.Activity.LoginActivity.EXTRA_DOCUMENTO;
@@ -16,6 +29,44 @@ import static com.iglesias.c.appgym.Activity.LoginActivity.EXTRA_URL_IMAGEN;
 public class MainActivity extends AppCompatActivity {
     private TextView txtNombre, txtDias, txtDocumento;
     ImageView imgUsr;
+    Button btnEntrar;
+    boolean onoff = false;
+
+    private final BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            switch (intent.getAction()) {
+                case UsbService.ACTION_USB_PERMISSION_GRANTED:
+                    Toast.makeText(context, "USB Ready", Toast.LENGTH_SHORT).show();
+                    break;
+                case UsbService.ACTION_USB_PERMISSION_NOT_GRANTED:
+                    Toast.makeText(context, "USB Permission not granted", Toast.LENGTH_SHORT).show();
+                    break;
+                case UsbService.ACTION_NO_USB:
+                    Toast.makeText(context, "NO USB Connected", Toast.LENGTH_SHORT).show();
+                    break;
+                case UsbService.ACTION_USB_DISCONNECTED:
+                    Toast.makeText(context, "USB desconnected", Toast.LENGTH_SHORT).show();
+                    break;
+                case UsbService.ACTION_USB_NOT_SUPPORTED:
+                    Toast.makeText(context, "USB device not supported", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+    };
+
+    private UsbService usbService;
+    private final ServiceConnection usbConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName arg0, IBinder arg1) {
+            usbService = ((UsbService.UsbBinder) arg1).getService();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            usbService = null;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,5 +95,65 @@ public class MainActivity extends AppCompatActivity {
         txtDocumento = findViewById(R.id.id_txt_documento);
         txtNombre = findViewById(R.id.id_txt_nombre);
         imgUsr = findViewById(R.id.imagen_usr);
+        btnEntrar = findViewById(R.id.id_btn_entrar);
+
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (onoff == false) {
+                    String dato = "1";
+                    usbService.write(dato.getBytes());
+                    btnEntrar.setText("APAGAR");
+                    onoff = true;
+                } else {
+                    String dato = "0";
+                    usbService.write(dato.getBytes());
+                    btnEntrar.setText("PRENDER");
+                    onoff = false;
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setFilters();  // Start listening notifications from UsbService
+        startService(UsbService.class, usbConnection, null); // Start UsbService(if it was not started before) and Bind it
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(mUsbReceiver);
+        unbindService(usbConnection);
+    }
+
+    private void startService(Class<?> service, ServiceConnection serviceConnection, Bundle extras) {
+        if (!UsbService.SERVICE_CONNECTED) {
+            Intent startService = new Intent(this, service);
+            if (extras != null && !extras.isEmpty()) {
+                Set<String> keys = extras.keySet();
+                for (String key : keys) {
+                    String extra = extras.getString(key);
+                    startService.putExtra(key, extra);
+                }
+            }
+            startService(startService);
+        }
+        Intent bindingIntent = new Intent(this, service);
+        bindService(bindingIntent, serviceConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    private void setFilters(){
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(UsbService.ACTION_USB_PERMISSION_GRANTED);
+        filter.addAction(UsbService.ACTION_NO_USB);
+        filter.addAction(UsbService.ACTION_USB_DISCONNECTED);
+        filter.addAction(UsbService.ACTION_USB_NOT_SUPPORTED);
+        filter.addAction(UsbService.ACTION_USB_PERMISSION_NOT_GRANTED);
+        registerReceiver(mUsbReceiver, filter);
+
     }
 }
